0819860059a0f0f369680cd712023e457c2f8b07
AlcidesDono2904 <alcidesjimenezcarrillo@gmail.com>
2024-04-13 17:35:00 -0600

    Clase categoria agregada

---
+++ I_Proyecto_Programacion/I_Proyecto_Programacion/Categoria.cpp
@@ -0,0 +1,11 @@
+#include "Categoria.h"
+
+Categoria::~Categoria() {
+}
+
+double Categoria::porceGanancia(std::string categ) {
+	if (categ == "01") return 1.15;
+	if (categ=="02") return 1.20;
+	if (categ == "03") return 1.25;
+	else throw "Error: categoria inexistente";
+}
