b607dddcd240f62c58c9fc79fa81867a5e4b107d
AlcidesDono2904 <alcidesjimenezcarrillo@gmail.com>
2024-04-14 23:45:03 -0600

    Implementacion de metodos de reportes y mantenimiento

--- I_Proyecto_Programacion/I_Proyecto_Programacion/Minisuper.h
+++ I_Proyecto_Programacion/I_Proyecto_Programacion/Minisuper.h
@@ -13,9 +13,18 @@ public:
 	virtual ~Minisuper();
 	ListaEnlazada<Producto>* getProductos();
 	ListaEnlazada<Venta>* getVentas();
+	//mantenimiento productos
 	void ingresarProducto(Producto* producto);
 	Producto* eliminarProducto(string codigo);
 	//void modificarProducto(string codigo, Producto* producto);
-	
+	//reporte productos
+	std::string reportarProductos();
+	std::string reportarCategoria(std::string categoria);
+	//std::string reportarDebajoExistencia(); ??
+	//mantenimiento ventas
+	//std::string generarVenta(Venta* venta,std::string cedula);
+	//reporte ventas
+	std::string reportarVentas(std::string cedula);
+	//reportar 5 mejores cedulas
 };
 #endif
